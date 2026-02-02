8ff6ba3a1741ff571678b26fab868e6238dc67b0
Casimir Wallwitz <casimir.wallwitz@tum.de>
2026-02-02 04:52:41 +0100

    add configurable placeholder for titiler

--- server/main.py
+++ server/main.py
@@ -15,7 +15,7 @@ app.add_middleware(
 )
 
 def fixed_path(url: str = None):
-    return "/home/casimir/UNI/WS_25/osmviz/server/euro-dem-tif/data/eurodem.tif"
+    return "YOURPATH"
 
 # Create a TilerFactory for Cloud-Optimized GeoTIFFs
 cog = TilerFactory(path_dependency=fixed_path)
